efc7d9c7fb9a08ef7de940d257045294e350f7dc
LuisPereira-20 <151740701+LuisPereira-20@users.noreply.github.com>
2024-08-24 10:20:47 -0400

    f

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+function Producto({ producto }) {
+    const [carrito, setCarrito] = useState(() => {
+        const carritoGuardado = localStorage.getItem('carrito');
+        return carritoGuardado ? JSON.parse(carritoGuardado) : [];
+    });
+    const handleAgregarAlCarrito = () => {
+      setCarrito([...carrito, producto]);
+      localStorage.setItem('carrito', JSON.stringify([...carrito, producto]));
+      console.log(carrito);
+    };
+  
+    return (
+      <div>
+        {/* Información del producto */}
+        <button onClick={handleAgregarAlCarrito}
+        className='bg-gray-200 rounded-xl h-fit w-fit p-4 gap-4 mx-4 my-4 hover:bg-gray-400'>Agregar al carrito</button>
+      </div>
+    );
+  }
+  export default Producto;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Exhibicion from './components/exhibidor.jsx'
-import Nav from './components/nav.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Inicio from './pages/inicio.jsx'
+import Productos from './pages/Productos.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-    <div className=''>
-    <Nav />
-    </div>
-    <div class='grid grid-cols-4 (0,1fr) gap-8 p-4 bg-gray-200'>
-    <Exhibicion />
-    <Exhibicion />
-    <Exhibicion />
-    <Exhibicion />
-    </div>
-    <div>
-    <Exhibicion />
-    </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="productos" element={<Productos />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
