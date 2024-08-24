00996d743d1e07ee84008d26716bada9e826e21e
LuisPereira-20 <151740701+LuisPereira-20@users.noreply.github.com>
2024-08-24 10:29:17 -0400

    Create Divs.jsx

---
+++ frontend/src/components/Divs.jsx
@@ -0,0 +1,12 @@
+import Producto from "./button";
+
+function Div () {
+    return(
+        <div>
+            <h1>Producto</h1>
+            <Producto />
+        </div>
+    )
+}
+
+export default Div
