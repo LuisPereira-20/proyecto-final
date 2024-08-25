b54dd4476657f8d6cc0acb11b9a135460309bcd3
LuisPereira-20 <151740701+LuisPereira-20@users.noreply.github.com>
2024-08-25 12:40:30 -0400

    Update ruta_usuario.js

--- backend/Routes/ruta_usuario.js
+++ backend/Routes/ruta_usuario.js
@@ -1,4 +1,16 @@
 import  {Router} from 'express';
+import multer from 'multer';
+const guardar = multer.diskStorage({
+    destination: function (req, file, cb)  {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb)  {
+        const uniconombre = Date.now() + '-' + file.originalname;
+        cb(null, uniconombre);
+    },
+});
+
+const upload = multer({ storage, limits: {files: 1} });
 const router  = Router();   
 
 import { getUsuarios, getUsuario, postUsuario, Actualizar_Usuario, Eliminar_Usuario, submitImg} from "../Controller/Controlador_usuario.js";
